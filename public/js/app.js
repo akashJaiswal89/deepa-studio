fe90f9317b15afa8470d5acfa2a97940e09a6885
akashJaiswal89 <73360527+akashJaiswal89@users.noreply.github.com>
2020-12-24 00:00:23 +0530

    complete home

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,30 @@ const navControl = document.querySelector('.navOpen');
 navControl.addEventListener('click', () => {
     const navOpen = document.querySelector('.leftNavIn')
     const navbarLeft = document.querySelector('.navbarLeft')
+    const navNone = document.querySelector('.rightNav')
 
     if (navOpen.classList.value.includes("navNone")) {
         navOpen.classList.remove('navNone')
+        navNone.classList.remove('navNone')
         navbarLeft.classList.add('navOn')
     } else {
         navOpen.classList.add('navNone')
         navbarLeft.classList.remove('navOn')
+        navNone.classList.add('navNone')
+    }
+})
+const navNone = document.querySelector('.rightNav').addEventListener('click', () => {
+    const navOpen = document.querySelector('.leftNavIn')
+    const navbarLeft = document.querySelector('.navbarLeft')
+    const navNone = document.querySelector('.rightNav')
+
+    if (navOpen.classList.value.includes("navNone")) {
+        navOpen.classList.remove('navNone')
+        navNone.classList.remove('navNone')
+        navbarLeft.classList.add('navOn')
+    } else {
+        navOpen.classList.add('navNone')
+        navbarLeft.classList.remove('navOn')
+        navNone.classList.add('navNone')
     }
 })
